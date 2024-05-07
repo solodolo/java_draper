fe321dccdff5e5626073b935b8667bd523d6a0cc
dmmettlach <drew@mettlach.codes>
2024-05-06 20:51:10 -0700

    Get basic methods working

---
+++ src/main/java/codes/mettlach/Presenter.java
@@ -0,0 +1,13 @@
+package codes.mettlach;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Presenter {
+    Class value();
+    String instanceName();
+}
