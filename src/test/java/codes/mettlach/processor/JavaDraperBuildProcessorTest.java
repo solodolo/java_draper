686b86f10353009638024a2c49e689d04b8676b6
dmmettlach <drew@mettlach.codes>
2024-06-06 22:14:55 -0700

    Restore test source file after test is complete

--- src/test/java/codes/mettlach/processor/JavaDraperBuildProcessorTest.java
+++ src/test/java/codes/mettlach/processor/JavaDraperBuildProcessorTest.java
@@ -1,5 +1,7 @@
 package codes.mettlach.processor;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.google.testing.compile.Compilation;
@@ -8,9 +10,27 @@ import com.google.testing.compile.Compilation.Status;
 import static com.google.testing.compile.Compiler.javac;
 import static org.junit.Assert.assertEquals;
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class JavaDraperBuildProcessorTest {
+    private final Path originalFooFilePath = Paths.get("src/test/java/codes/mettlach/processor/test_src/FooPresenter.java");
+    private final Path backupFooFilePath = Paths.get("src/test/java/codes/mettlach/processor/test_src/FooPresenter.bak.java");
+    @Before
+    public final void setUp() throws IOException {
+        Files.copy(originalFooFilePath, backupFooFilePath, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    @After
+    public final void tearDown() throws IOException {
+        Files.deleteIfExists(originalFooFilePath);
+        Files.move(backupFooFilePath, originalFooFilePath);
+    }
+
     @Test
     public void annotatedClassCompiles() throws MalformedURLException {
         final JavaDraperBuildProcessor processor = new JavaDraperBuildProcessor();
